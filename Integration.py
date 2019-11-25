eed44af6d4eac621d9c41427565353f6992fa06e
JoshCowan17 <Jccjr17@gmail.com>
2019-11-24 22:26:04 -0500

    Update Integration.py

--- Integration.py
+++ Integration.py
@@ -1,58 +1,338 @@
-#Joshua Cowan\n
-#I want to create a buzzfeed quiz that is fun and simple for my friends and family to enjoy. Afterwards I will pool the results.
-print (2+3)
-print ("Hello! Welcome to 'Which Spongebob character are you?'")
-#I need to create a defined system for the Q/A format
-class question:
-    def __init__(self, prompt, selection):
-        self.prompt = prompt
-        self.prompt = selection
-question_prompts = [
-    "What do you like to do for fun?\n(a) Sleeping\n(b) Playing Music\n(c) Reading\n(d) Jellyfishing\n(e) Watch TV"
-#Keeping this template in mind, I should have roughly 4-7 answers for each question.
-#Sponge, Pat, Squid, Sandy, Gary, Krabs, Plankton
-    "Where is your ideal place to work?"
-    "If you could live anywhere in the world, where would it be?"
-    "Pick a word"
-    "What is your favorite food?"
-    "What is your favorite Spongebob episode?"
-    "You have a big assignment coming up. How do you deal with the workload?"
+print("Welcome! To the Spongebob Squarepants quiz!"), '\n'
+print(
+    'Here you will be asked a series of questions regarding food, activities,'
+    ' etc. to see who you resemble most from '
+    'Spongebob!')
 
-]
 
-def run_test(question):
-        spongebobScore= 0
-        squidwardScore= 0
-        patrickScore= 0
-        sandyScore= 0
-        garyScore= 0
-        krabsScore= 0
-        planktonScore= 0
-        
-        while not quiz_end:
+def invalid_input():
+    print("Invalid input, please make sure there are no grammatical errors or"
+          " incorrect capitals. :)")
+
+
+quiz_end = False
+
+questions = int(1)
+
+patrick = 0
+sandy = 0
+mrkrabs = 0
+plankton = 0
+spongebob = 0
+squidward = 0
+
+while not quiz_end:
     if questions == 1:
-        print("which of these foods would you choose?")
-        input(print("Money, Chum, IceCream, or KrabbyPatties")
+        print()
+        print("Which of these foods would you choose?"),
+        print(input("Money, Chum, Ice Cream, or Krabby Patties?"))
+        print()
 
-        if input == Money or money
-        (mrKrabs + 1)
+        if input == "Money" or input == "money":
+            mrkrabs = 2
 
-        else input == Chum or chum
-        (plankton + 1)
+        elif input == "Chum" or input == "chum":
+            plankton = 2
 
-        if input == Icecream or icecream
-        (patrick + 1)
+        elif input == "Ice Cream" or input == "ice cream":
+            patrick = 2
 
-        else input == Krabbypatties or krabbypatties or KrabbyPatties
-        (spongebob + 1)
-        (sandy + 1)
+        elif input == "Krabby patties" or input == "Krabby Patties":
+            spongebob = 2
+            sandy = 2
 
-
-    if questions == 2
-    print("What is your favorite color?")
-
+        else:
+            print("this selection is invalid, please correct any spelling"
+                  " errors, or choose a valid response ")
+            questions == 1
     questions += 1
-    #Figure out how to fix test(quiz question loop)
-for question in question_prompts:
-    selection = input(question.prompt)
-    print ("Correct")
+#The transition here is hard to keep working while also keeping the Q loop
+#Next time doing massive loops, try to utilize classes to make working
+#On the loop easier instead of having to change EVERYTHING
+    if questions == 2:
+        print()
+        print("Which of the following is your favorite pass time?"),
+        print(input("Money, Cooking, Science, Jellyfishing, or Sleeping?"))
+
+        if input == "Money" or input == "money":
+            mrkrabs + int(1)
+
+        elif input == "science" or input == "Science":
+            plankton + int(1)
+            sandy + int(1)
+
+        elif input == "Sleeping" or input == "sleeping":
+            patrick + int(1)
+
+        elif input == "Cooking" or input == "cooking":
+            spongebob + int(1)
+
+        elif input == "Jelly Fishing" or input == "jellyfishing":
+            spongebob + int(1)
+            patrick + int(1)
+
+        else:
+            print("this selection is invalid, please correct any spelling "
+                        "errors, or choose a valid response :)")
+            questions = 2
+    questions += 1
+
+    if questions == 3:
+        print()
+        print("Pick a word?"),
+        print(input("Money, Cheerful, Imagination, Science, Music"))
+
+        if input == "Money" or input == "money":
+            mrkrabs + int(1)
+
+        elif input == "Cheerful" or input == "cheerful":
+            spongebob + int(1)
+
+        elif input == "Imagination" or input == "imagination":
+            patrick + int(1)
+            spongebob + int(1)
+
+        elif input == "Science" or input == "science":
+            sandy + int(1)
+
+        elif input == "Music" or input == "music":
+            squidward + int(1)
+
+        else:
+            print("this selection is invalid, please correct any spelling "
+                        "errors, or choose a valid response :)")
+            questions = 3
+    questions += 1
+
+    if questions == 4:
+        print()
+        print("Pick a color?"),
+        print(input("Yellow, Pink, Green, Blue, Red"))
+
+        if input == "Yellow" or input == "yellow":
+            spongebob + int(1)
+
+        elif input == "Pink" or input == "pink":
+            patrick + int(1)
+
+        elif input == "Green" or input == "green":
+            mrkrabs + int(1)
+
+        elif input == "Blue" or input == "blue":
+            squidward + int(1)
+
+        elif input == "Red" or input == "Red":
+            mrkrabs + int(1)
+
+        else:
+            print("this selection is invalid, please correct any spelling "
+                        "errors, or choose a valid response :)")
+            questions = 4
+    questions += 1
+
+    if questions == 5:
+        print()
+        print("What's your favorite Holiday?"),
+        print(input("Leif Erikson Day, Annoy Squidward Day, Halloween,"
+                    " Give Mr. Krabs Money Day, Opposite Day"))
+
+        if input == "Leif Erikson Day":
+            patrick + int(1)
+
+        elif input == "Annoy Squidward Day":
+            spongebob + int(1)
+
+        elif input == "Halloween":
+            squidward + int(1)
+
+        elif input == "Give Mr. Krabs Money Day":
+            mrkrabs + int(1)
+
+        elif input == "Opposite Day":
+            plankton + int(1)
+
+        else:
+            print("this selection is invalid, please correct any spelling "
+                  "errors, or choose a valid response :)")
+            questions = 5
+    questions += 1
+
+    if questions == 6:
+        print()
+        print("How would your friends describe you?")
+        print(input("Stupid, Creative, Smart, Stingy, Grumpy"))
+
+        if input == "Stupid":
+            patrick + int(1)
+
+        elif input == "Creative":
+            plankton + int(1)
+
+        elif input == "Smart":
+            sandy + int(1)
+
+        elif input == "Stingy":
+            mrkrabs + int(1)
+
+        elif input == "Grumpy":
+            squidward + int(1)
+
+        else:
+            print("this selection is invalid, please correct any spelling "
+                  "errors, or choose a valid response :)")
+            questions = 6
+    questions += 1
+
+    if questions == 7:
+        print()
+        print("Which Bikini Bottom Resident would you like to hang out with?")
+        print(input("Karen, Larry, Mrs. Puff, Gary, Nobody"))
+
+        if input == "Karen":
+            plankton + int(1)
+
+        elif input == "Larry":
+            patrick + int(1)
+
+        elif input == "Mrs. Puff":
+            mrkrabs + int(1)
+
+        elif input == "Gary":
+            spongebob + int(1)
+
+        elif input == "Nobody":
+            squidward + int(1)
+
+        else:
+            print("this selection is invalid, please correct any spelling "
+                  "errors, or choose a valid response :)")
+            questions = 7
+    questions += 1
+
+    if questions == 8:
+        print()
+        print("Pick an instrument")
+        print(input("Clarinet, Mayonnaise, Drums, Violin, Guitar"))
+
+        if input == "Clarinet":
+            squidward + int(1)
+
+        elif input == "Mayonnaise":
+            patrick + int(1)
+
+        elif input == "Drums":
+            sandy + int(1)
+
+        elif input == "Violin":
+            mrkrabs + int(1)
+
+        elif input == "Guitar":
+            spongebob += 1
+
+        else:
+            print("this selection is invalid, please correct any spelling "
+                  "errors, or choose a valid response :)")
+            questions = 8
+    questions += 1
+
+    if questions == 9:
+        print()
+        print("What's your favorite item?")
+        print(input("A Krabby Patty, Secret Box, Chocolate, Walnuts"))
+
+        if input == "A Krabby Patty":
+            mrkrabs + int(1)
+            plankton + int(1)
+
+        elif input == "Secret Box":
+            patrick + int(1)
+
+        elif input == "Chocolate":
+            spongebob + int(1)
+
+        elif input == "Walnuts":
+            sandy + int(1)
+
+        else:
+            print("this selection is invalid, please correct any spelling "
+             "errors, or choose a valid response :)")
+            questions = 9
+    questions += 1
+
+    if questions == 10:
+        print()
+        print("Are you ready?")
+        print(input("I'm Ready. I'm Ready!, Ready for what?, Oh no.,"
+                    "Ready to count me money!, Ready for the secret formula!"))
+
+        if input == "I'm ready. I'm ready!":
+            spongebob + int(1)
+
+        elif input == "Ready for what?":
+            patrick + int(1)
+
+        elif input == "Oh no.":
+            squidward + int(1)
+
+        elif input == "Ready to count me money!":
+            mrkrabs + int(1)
+
+        elif input == "Ready for the secret formula":
+            plankton + int(1)
+
+        else:
+            print("this selection is invalid, please correct any spelling "
+             "errors, or choose a valid response :)")
+            questions = 10
+
+    quiz_end = True
+
+
+while quiz_end = True:
+    if (patrick > sandy
+            and patrick > mrkrabs
+            and patrick > squidward
+            and patrick > spongebob
+            and patrick > plankton):
+          print("You got Patrick Star! The lovable, albeit slow starfish.")
+    
+    elif (sandy > patrick
+          and sandy > mrkrabs
+          and sandy > spongebob
+          and sandy > plankton
+          and sandy > squidward):
+          print("You got Sandy Cheeks! The smart and sassy squirrel")
+    
+    elif (spongebob > patrick
+        and spongebob > sandy
+        and spongebob > plankton
+        and spongebob > mrkrabs
+        and spongebob > squidward):
+          print("You got Spongebob Squarepants! The Happy-go Lucky Sponge.")
+    
+    elif (plankton > spongebob
+          and plankton > squidward
+          and plankton > sandy
+          and plankton > patrick
+          and plankton > mrkrabs):
+            print("You got Plankton! The Evil genius who always wants the"
+                  "secret formula")
+          
+    elif (squidward > spongebob
+        and squidward > patrick 
+        and squidward > sandy 
+        and squidward > mrkrabs 
+        and squidward > plankton):
+            print("You got Squidward! The depressed artist type.")
+    
+    elif (mrkrabs > spongebob
+        and mrkrabs > squidward
+        and mrkrabs > sandy
+        and mrkrabs > plankton
+        and mrkrabs > patrick):
+            print("You got Mr. Krabs! The money obsessed owner of"
+                  "the Krusty Krab.")
+    else:
+        print("Sorry, you do not have a strong leaning towards any of"
+              "the characters, please try again!")
+print ("Thank you for taking my Spongebob quiz!")
